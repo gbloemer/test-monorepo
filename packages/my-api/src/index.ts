bf0e56e3e58fbe14b969e983bc9eed463e3401e0
Guenter Bloemer <guenterbloemer@gmail.com>
2023-11-07 18:21:54 -0300

    fixed tsconfigs

--- packages/my-api/src/index.ts
+++ packages/my-api/src/index.ts
@@ -1,6 +1,6 @@
-import { User } from "@my-shared/models/user"
-import { getUser, User2 } from "@my-logic/services/users.service"
-import userRoute from "@routes/user"
+import { User } from "my-shared/src/models/user"
+import { getUser, User2 } from "my-logic/src/services/users.service"
+import userRoute from "./routes/user"
 
 const user: User = getUser()
 //const user: User2 = { name: 'test' }
